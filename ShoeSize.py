a0d5a83c23d2aec92703fec7c01e6f2ec69bfa8a
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-01 10:17:29 +1200

    Shoe Size casting exercise

---
+++ ShoeSize.py
@@ -0,0 +1,16 @@
+# ShoeSize.Py
+# @ Author: Helen Thomas
+# Date: 01.08.23
+
+# variable set to integer
+my_size= 8.5
+
+print("The type of my_size is ", type(my_size),"\n")
+print("my_size is ", my_size,"\n")
+
+print("Casting to a float...\n")
+# variable cast to float
+my_size = float(my_size)
+
+print("The type of my_size is now", type(my_size),"\n")
+print("my_size is ", my_size, "\n")
