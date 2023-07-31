6434a8ad1b130616f0f0f973487968db3c2bbf03
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-01 09:58:08 +1200

    Exercises added from Pycharm
    Exercises from course

---
+++ Baby.py
@@ -0,0 +1,14 @@
+# Baby.py
+# @ author: Helen Thomas
+# Date:1.08.23
+
+#Boolean exercise
+
+import random
+
+male = False
+male = bool(random.randint(0, 1))
+if (male):
+    print ("We will use the name Rangi")
+else:
+    print ("we will use name Anihera")
