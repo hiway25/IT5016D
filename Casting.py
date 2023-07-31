037ad47ecab8cf9e4f3867207a9e43d241c41d0b
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-01 10:07:02 +1200

    Casting exercsie

---
+++ Casting.py
@@ -0,0 +1,16 @@
+#Casting.Py
+# @ Author: Helen Thomas
+# Date: 01.08.23
+
+# variable set to integer
+my_score = 201
+
+print("The type of my_score is ", type(my_score),"\n")
+print("my_score is ", my_score,"\n")
+
+print("Casting to a strong...\n")
+# variable cast to string
+my_score = str(my_score)
+
+print("The type of my_score is now", type(my_score),"\n")
+print("my_score is ", my_score, "\n")
