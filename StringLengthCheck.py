9a3dd45928ab634b643226d7c3b101fa7efc7833
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-09 10:02:53 +1200

    Check if input length is valid - eg passwords

---
+++ StringLengthCheck.py
@@ -0,0 +1,10 @@
+# StringLengthCheck.py
+# Author: Helen Thomas
+# Date: 09.08.2023
+
+length = input("Please enter a test string:  \n")
+
+if len(length) < 6:
+    print("Your string is too short.")
+    print("Please enter a string with at least 6 characters.")
+
