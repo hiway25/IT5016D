5eeb1223a8e7507e88271e0776076229daad037a
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-09 09:57:04 +1200

    Determine if an input is odd or even

---
+++ OddEven.py
@@ -0,0 +1,13 @@
+# OddEven.py
+# Author: Helen Thomas
+# Date: 09.08.2023
+
+#prompt user to enter number / test if odd or even
+number = int(input("Please enter an integer: \n"))
+
+
+if number % 2 == 0:
+    print("Your number is even.")
+else:
+    print("Your number is odd.")
+
