bfff7e07e1b908b0cf64ef2286021ee4991d585d
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-10 12:53:44 +1200

    Making a user enter something into a field

---
+++ NameLoop.py
@@ -0,0 +1,17 @@
+# NameLoop.py
+#  Author: Helen Thomas
+#  Date: 10.08.2023
+
+name = ""
+
+while len(name) == 0:
+    name = input("Enter your name: ")
+
+print("Hello " +name )
+
+"""
+Could also run the same thing by 
+name = None
+
+while not name:
+    name = input...yadda yadda"""
