995a690ee3c8cb24cb7d9923b7672f1e6156a3b4
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-12 09:44:40 +1200

    Index return

---
+++ DaysWeekReo.py
@@ -0,0 +1,6 @@
+# DaysWeekReo
+#  Author: Helen Thomas
+# Date:  11.08.2023
+
+maoridays = ["Raahina", "Raatuu", "Raaapa", "Raapare", "Raamare", "Raahoroi", "Raatapu"]
+print(maoridays [1], maoridays[4])
