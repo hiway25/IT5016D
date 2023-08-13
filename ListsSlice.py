ba30228e5241c3996eba743ce555f979839735e8
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-14 10:16:01 +1200

    Slicing Lists
    Exercise showing how to return certain list items.
    Slicing lists

---
+++ ListsSlice.py
@@ -0,0 +1,16 @@
+#  ListsSlice.py
+#  Author: Helen Thomas
+#  Date: 12.08.2023
+
+my_list = ['k','e','t','e','m','a','r','a','e']
+# elements 3rd to 5th
+print(my_list[2:5])
+
+# elements beginning to 4th
+print(my_list[:-5])
+
+#elements 6th to end
+print(my_list[5:])
+
+#elements beginning to end
+print(my_list[:])
