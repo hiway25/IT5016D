07901a0bd55d4224a45473d05b6e327474ba570f
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-14 10:28:26 +1200

    Intro to functions
    Invoking the function

---
+++ Function1.py
@@ -0,0 +1,15 @@
+#  Function1.py
+#  Author:  Helen Thomas
+#  Date: 14.08.2023
+
+
+
+def show_hello():
+    print("Hello there, this is my very first function!!!\n\n")
+    
+print("Testing my first user defined function...\n\n")
+
+# invoking the function
+show_hello()
+# Invoking the function again
+show_hello()
