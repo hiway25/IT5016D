5f913b4be4db18f95d2ab1621eac27e071dfbb1a
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-14 11:44:53 +1200

    Simple addition function

---
+++ SimpleAdditionFunction.py
@@ -0,0 +1,10 @@
+# SimpleAdditionFunction.py
+# Author: Helen Thomas
+# DAte: 14.08.2023
+
+def simple_addition(num1, num2):
+    answer = num1 + num2
+    print("num1 is ", num1)
+    print(answer)
+
+simple_addition(5,3)
