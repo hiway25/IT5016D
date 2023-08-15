fa6e041dbe68d5035fd9751b246cfb1bbc2a4171
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-16 09:43:32 +1200

    Function to return multiplication

---
+++ ReturnFuncMultiply.py
@@ -0,0 +1,10 @@
+#  RetunrFuncMultiply.py
+#  Author: Helen Thomas
+#  Date: 15.08.2023
+
+# functions send Python values/objects back to the caller
+def multiply(num1,num2):
+    return num1 * num2
+
+x = multiply(6,8)
+print(x)
