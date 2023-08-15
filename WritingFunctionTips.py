46e43406db3dde5732fb755cf01cee21c878f87e
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-16 09:41:46 +1200

    No code: Just tips for writing functions

---
+++ WritingFunctionTips.py
@@ -0,0 +1,36 @@
+# WritingFunctionTips.py
+#  Author: Helen Thomas
+#  DAte: 15.08.2023
+
+"""
+Keep it simple! Write a function to do only one thing,
+and do it well. If a function needs to do several things,
+then break it up into smaller functions.
+Keep functions short. 10 to 20 lines of code is the maximum
+needed per function. If you find that you need to write more
+than this then consider breaking up your function into
+several smaller functions.
+Use few function parameters. This makes code easier to read.
+Aim to use up to 3 parameters. If a function requires more,
+consider passing a container as a parameter instead.
+Use meaningful variable names and be consistent.
+Use grouping of code blocks to make code easier to read.
+Avoid deep nesting of statements. Deeply nested code is
+difficult to test and debug. Consider alternative approaches
+where possible.
+Always use the DRY principle. DRY stands for "Do Not Repeat Yourself".
+Where logic has been written in code, it should only be written once.
+If not then consider writing a function to reuse the logic.
+Limit the line length of code. Avoid writing horizontally
+long lines of code. You have practised how to do this for
+writing code that contains strings and condition statements.
+Our eyes are more comfortable when reading tall and narrow
+columns of text.
+
+Try to separate code and data. There are many ways of achieving this,
+ some of which have yet to be covered in the course.
+ Consider using functions to return data, rather than using print
+ statements within the functions.
+Refactor your code! Go back to your saved programs and
+improve upon them every time you learn new skills.
+"""
