72283fbe94905a2ee96e89ffbc08b00d300dc669
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-18 09:44:53 +1200

    Concantenation
    # Concatenation - combining two or more strings into a single string

---
+++ ConcantenationStrings.py
@@ -0,0 +1,15 @@
+# ConcantenationStrings.py
+# Author: Helen Thomas
+# Date: 17.08.2023
+
+# Concatenation - combining two or more strings into a single string
+
+string_1 = "Hello World"
+
+print("Using the join method to add a : between every char...\n{0}"
+      .format(":".join(string_1)),
+      "\n")
+
+print("Using the join method to add a whitespace between every char...\n{0}"
+      .format(" ".join(string_1)),
+      "\n")
