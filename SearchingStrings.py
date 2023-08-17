8bc9cea9c4b09e59c573254355a615756090027c
hiway25 <75404612+hiway25@users.noreply.github.com>
2023-08-18 09:39:00 +1200

    Simple string search exercise

---
+++ SearchingStrings.py
@@ -0,0 +1,8 @@
+# SearchingStrings.py
+#  Author: Helen Thomas
+# Date: 17.08.2023
+
+string_1 = "Whanau!"
+
+print("The third letter is '{0}'\n".format(string_1[2]))
+print("The length of the string is {0}".format(len(string_1)))
